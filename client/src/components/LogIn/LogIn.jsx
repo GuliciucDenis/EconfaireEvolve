c93b477cf2a8cfd866fefeabc92b435b8799aa5f
GuliciucDenis <Guliciuc.denis2003@gmail.com>
2024-08-22 11:15:49 +0300

    update

--- client/src/components/LogIn/LogIn.jsx
+++ client/src/components/LogIn/LogIn.jsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './LogIn.css';
 import Dots from '../../images/DecoratieColt.png';
 import EvolveLogo from '../../images/eVolvelogo.png';
 import LogInImage from '../../images/man.png';
+import useLogin from '../UseLogin.js/UseLogin';
 
-function LogIn() {
+const LogIn = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const { login, loading, error } = useLogin();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await login(email, password);
+  }
+
   return (
     <div className="login-background">
       <div className="login-content">
@@ -17,24 +27,31 @@ function LogIn() {
           <img src={LogInImage} className='man-ladder' alt="Man on ladder" />
           <div className='second-blob'></div>
           <div className='wrapper'>
-            <form className='login-form'>
-              <div className='input-box'>
-                <p>Name:</p>
-                <input type='text' placeholder='Name' required />
-              </div>
-              <div className='input-box'>
-                <p>Surname:</p>
-                <input type='text' placeholder='Surname' required />
-              </div>
+            <form className='login-form' onSubmit={handleSubmit}>
               <div className='input-box'>
                 <p>Email:</p>
-                <input type='email' placeholder='Email' required />
+                <input 
+                  type='email' 
+                  placeholder='Email' 
+                  required
+                  onChange={(e) => setEmail(e.target.value)}
+                  value={email}
+                />
               </div>
               <div className='input-box'>
                 <p>Password:</p>
-                <input type='password' placeholder='Password' required />
+                <input 
+                  type='password' 
+                  placeholder='Password' 
+                  required
+                  onChange={(e) => setPassword(e.target.value)}
+                  value={password}
+                />
               </div>
-              <button className='login-button'>Log In</button>
+              <button type="submit" className='login-button' disabled={loading}>
+                {loading ? 'Logging in...' : 'Log In'}
+              </button>
+              {error && <p className="error-message">{error}</p>}
             </form>
           </div>
         </main>
