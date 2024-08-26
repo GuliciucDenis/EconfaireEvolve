16e6017b0866e18d48fef328bd86042a400f2a3d
FlavianB <flaviburca@gmail.com>
2024-08-26 14:13:51 +0300

    profile page update

--- client/src/pages/profile/Profile.jsx
+++ client/src/pages/profile/Profile.jsx
@@ -9,30 +9,32 @@ const Profile = () => {
     <div className="profile-page">
       <Background />
       <Navbar/>
-      <h1 className="profile-title">Profile</h1>
-      <div className="profile-content">
-        <div className="image-and-modify-container">
-          <div className="profile-container">
-            <img className="profile-image" src={ProfilePic} alt="Profile Image" />
+      <div className="content">
+        <h1 className="profile-title">Profile</h1>
+        <div className="profile-content">
+          <div className="image-and-modify-container">
+            <div className="profile-container">
+              <img className="profile-image" src={ProfilePic} alt="Profile Image" />
+            </div>
+            <div className="modify-password-container">
+              <button className="modify-password-button">Modify password</button>
+            </div>
           </div>
-          <div className="modify-password-container">
-            <button className="modify-password-button">Modify password</button>
+          <div className="profile-table-container">
+            <div className="profile-table">
+              <table>
+                <tbody>
+                  <tr><td>First name</td><td>Prenume</td></tr>
+                  <tr><td>Last name</td><td>Nume</td></tr>
+                  <tr><td>Email</td><td>ecf@econfaire.ro</td></tr>
+                  <tr><td>Password</td><td>PAROLA</td></tr>
+                  <tr><td>Department</td><td>HR</td></tr>
+                </tbody>
+              </table>
+            </div>
           </div>
-        </div>
-        <div className="profile-table-container">
-          <div className="profile-table">
-            <table>
-              <tbody>
-                <tr><td>First name</td><td>Prenume</td></tr>
-                <tr><td>Last name</td><td>Nume</td></tr>
-                <tr><td>Email</td><td>ecf@econfaire.ro</td></tr>
-                <tr><td>Password</td><td>PAROLA</td></tr>
-                <tr><td>Department</td><td>HR</td></tr>
-              </tbody>
-            </table>
-          </div>
-        </div>
       </div>
+    </div>
     </div>
   );
 };
