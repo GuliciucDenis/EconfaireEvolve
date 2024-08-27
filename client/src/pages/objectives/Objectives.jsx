647e47d48bc9409a4a171df02b09efc6789ffe4b
GuliciucDenis <Guliciuc.denis2003@gmail.com>
2024-08-27 10:38:22 +0300

    CardComponent

--- client/src/pages/objectives/Objectives.jsx
+++ client/src/pages/objectives/Objectives.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 import Navbar from '../../components/common/navbar/Navbar'
 import Background from '../../components/background/Background';
+import Cardboard from '../../components/cardboard/Cardboard';
+
+import './Objectives.css';
 
 const Objectives = () => {
-  return <div>
-    <Background/>
-    <Navbar/>
-  </div>;
+  return (
+    <div>
+      <Background/>
+      <div className="cardboard-container">
+        <Cardboard/>
+        <Cardboard/>
+        <Cardboard/>
+      </div>
+      <Navbar/>
+    </div>
+  );
 };
 
 export default Objectives;
