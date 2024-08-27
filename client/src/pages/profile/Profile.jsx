002b2b1f5053c66e023b8e3d088c45eecbf3ff52
rZn24 <razvanhertanu@gmail.com>
2024-08-27 13:26:46 +0300

    added validation - create user form

--- client/src/pages/profile/Profile.jsx
+++ client/src/pages/profile/Profile.jsx
@@ -1,27 +1,59 @@
 import React from "react";
-import ProfilePic from '../../images/ProfilePic.png';
-import Background from '../../components/background/Background.jsx';
-import './Profile.css';
+import ProfilePic from "../../images/ProfilePic.png";
+import Background from "../../components/background/Background.jsx";
+import "./Profile.css";
 import Navbar from "../../components/common/navbar/Navbar";
-import ProfilePicHeader from '../../images/ProfilePicHeader.png';
-import ProfileTable from '../../components/profile/ProfileTable';
-import User from '../../components/common/user/User';
+import ProfilePicHeader from "../../images/ProfilePicHeader.png";
+import ProfileTable from "../../components/profile/ProfileTable";
+import User from "../../components/common/user/User";
+import ChangePasswordPopup from "../../components/common/ChangePassword/ChangePasswordPopup";
+import { useState } from "react";
 
 const Profile = () => {
+  const [isChangePasswordPopupOpen, setIsChangePasswordPopupOpen] =
+    useState(false);
+
   return (
     <div className="profile-page">
       <Background />
-      <img src={ProfilePicHeader} alt="Profile Pic Header" className="profile-pic-header" onClick={() => {window.location.href = '/profile';}}/>
-      <Navbar className="navbar"/>
-      <h3 className="profile-username" onClick={() => {window.location.href = '/profile';}}>Username</h3>
+      <img
+        src={ProfilePicHeader}
+        alt="Profile Pic Header"
+        className="profile-pic-header"
+        onClick={() => {
+          window.location.href = "/profile";
+        }}
+      />
+      <Navbar className="navbar" />
+      <h3
+        className="profile-username"
+        onClick={() => {
+          window.location.href = "/profile";
+        }}
+      >
+        Username
+      </h3>
       <h1 className="profile-title">Profile</h1>
       <div className="profile-content">
         <div className="image-and-modify-container">
-            <img className="profile-image" src={ProfilePic} alt="Profile Image" />
-            <button className="modify-password-button" onClick={() => {window.location.href = '/modifypassword';}}>Modify password</button>
+          <img className="profile-image" src={ProfilePic} alt="Profile Image" />
+          <button
+            className="modify-password-button-modifypasswordpage"
+            onClick={() => setIsChangePasswordPopupOpen(true)}
+          >
+            Change Password
+          </button>
         </div>
-        <ProfileTable/>
-    </div>
+        <ProfileTable />
+        <ChangePasswordPopup
+          isOpen={isChangePasswordPopupOpen}
+          onClose={() => setIsChangePasswordPopupOpen(false)}
+          onChangePassword={(oldPassword, newPassword) => {
+            // Implement the password change logic here
+            console.log("Changing password", oldPassword, newPassword);
+          }}
+        />
+      </div>
     </div>
   );
 };
