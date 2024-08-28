7b5d542cce1e491b39f932f4ff06506f0c2e5d1f
rZn24 <razvanhertanu@gmail.com>
2024-08-28 14:03:10 +0300

    added create objective popup

--- client/src/pages/AddObjectives/AddObjectives.jsx
+++ client/src/pages/AddObjectives/AddObjectives.jsx
@@ -1,24 +1,34 @@
 import React, { useEffect, useState } from "react";
-import Navbar from '../../components/common/navbar/Navbar';
-import Background from '../../components/background/Background';
-import AddObjectivesCard from '../../components/AddObjectives/AddObjectivesCard';
-import User from '../../components/common/user/User';
-import './AddObjectives.css';
-import { getObjectivesByUserToken, getObjectiveById, deleteObjectiveById } from "../../services/objectiveService";
+import Navbar from "../../components/common/navbar/Navbar";
+import Background from "../../components/background/Background";
+import AddObjectivesCard from "../../components/AddObjectives/AddObjectivesCard";
+import User from "../../components/common/user/User";
+import "./AddObjectives.css";
+import {
+  getObjectivesByUserToken,
+  getObjectiveById,
+  deleteObjectiveById,
+} from "../../services/objectiveService";
+import AddObjectivesPopup from "../../components/common/AddObjectivesPopup/AddObjectivesPopup";
 
 const AddObjectives = () => {
-  const [selectedRecommendedObjective, setSelectedRecommendedObjective] = useState(null);
-  const [selectedExistingObjective, setSelectedExistingObjective] = useState(null);
+  const [selectedRecommendedObjective, setSelectedRecommendedObjective] =
+    useState(null);
+  const [selectedExistingObjective, setSelectedExistingObjective] =
+    useState(null);
   const [newObjectiveName, setNewObjectiveName] = useState("");
   const [userObjectives, setUserObjectives] = useState([]);
+  const [isCreatePopupOpen, setIsCreatePopupOpen] = useState(false);
 
   useEffect(() => {
     const fetchUserObjectives = async () => {
       const userObjectiveIds = await getObjectivesByUserToken();
-      const userObjectives = await Promise.all(userObjectiveIds.map(async (id) => {
-        const objective = await getObjectiveById(id);
-        return objective;
-      }));
+      const userObjectives = await Promise.all(
+        userObjectiveIds.map(async (id) => {
+          const objective = await getObjectiveById(id);
+          return objective;
+        })
+      );
       setUserObjectives(userObjectives);
     };
     fetchUserObjectives();
@@ -26,22 +36,22 @@ const AddObjectives = () => {
 
   const objectivesData = [
     {
-        title: "Recommended Objectives",
-        content: ["Objective 1", "Objective 2", "Objective 3"]
+      title: "Recommended Objectives",
+      content: ["Objective 1", "Objective 2", "Objective 3"],
     },
     {
-        title: "Existing Objectives",
-        content: ["Objective 4", "Objective 5", "Objective 6"]
-    }
-  ]
+      title: "Existing Objectives",
+      content: ["Objective 4", "Objective 5", "Objective 6"],
+    },
+  ];
 
   const handleRecommendedObjectiveClick = (index) => {
     // Reset existing objective selection
     setSelectedExistingObjective(null);
     if (index === selectedRecommendedObjective) {
-        setSelectedRecommendedObjective(null);
+      setSelectedRecommendedObjective(null);
     } else {
-        setSelectedRecommendedObjective(index);
+      setSelectedRecommendedObjective(index);
     }
   };
 
@@ -49,9 +59,9 @@ const AddObjectives = () => {
     // Reset recommended objective selection
     setSelectedRecommendedObjective(null);
     if (index === selectedExistingObjective) {
-        setSelectedExistingObjective(null);
-    } else {    
-        setSelectedExistingObjective(index);
+      setSelectedExistingObjective(null);
+    } else {
+      setSelectedExistingObjective(index);
     }
   };
 
@@ -60,9 +70,7 @@ const AddObjectives = () => {
   };
 
   const handleCreateObjective = () => {
-    // Logic to create a new objective
-    console.log("Creating new objective:", newObjectiveName);
-    setNewObjectiveName("");
+    setIsCreatePopupOpen(true);
   };
 
   const handleDeleteObjective = async () => {
@@ -70,42 +78,48 @@ const AddObjectives = () => {
       const objectiveToDelete = userObjectives[selectedExistingObjective];
       console.log("Deleting objective:", objectiveToDelete);
       await deleteObjectiveById(objectiveToDelete.id); // Ensure this call waits for the deletion
-      setUserObjectives(userObjectives.filter((_, index) => index !== selectedExistingObjective));
+      setUserObjectives(
+        userObjectives.filter((_, index) => index !== selectedExistingObjective)
+      );
       setSelectedExistingObjective(null); // Reset the selection
     }
   };
 
   return (
     <div className="add-objectives-container">
-      <Background/>
+      <Background />
       <User />
       <div className="content-wrapper">
         <div className="user-info">User selected: Id Username</div>
         <div className="main-content">
           <div className="cards-container">
-            <AddObjectivesCard 
-              title={objectivesData[0].title} 
+            <AddObjectivesCard
+              title={objectivesData[0].title}
               content={objectivesData[0].content.map((objective, index) => (
-                <div 
-                  key={index} 
+                <div
+                  key={index}
                   onClick={() => handleRecommendedObjectiveClick(index)}
-                  className={`objective-item ${index === selectedRecommendedObjective ? 'selected' : ''}`}
+                  className={`objective-item ${
+                    index === selectedRecommendedObjective ? "selected" : ""
+                  }`}
                 >
                   {objective}
                 </div>
-              ))} 
+              ))}
             />
-            <AddObjectivesCard 
-              title='Existing Objectives' 
+            <AddObjectivesCard
+              title="Existing Objectives"
               content={userObjectives.map((objective, index) => (
-                <div 
-                  key={index} 
+                <div
+                  key={index}
                   onClick={() => handleExistingObjectiveClick(index)}
-                  className={`objective-item ${index === selectedExistingObjective ? 'selected' : ''}`}
+                  className={`objective-item ${
+                    index === selectedExistingObjective ? "selected" : ""
+                  }`}
                 >
                   {objective.title}
                 </div>
-              ))} 
+              ))}
             />
           </div>
           <div className="decision-container">
@@ -116,7 +130,12 @@ const AddObjectives = () => {
               {selectedExistingObjective !== null && (
                 <>
                   <button className="action-button">Edit Subobjectives</button>
-                  <button className="action-button delete-button" onClick={handleDeleteObjective}>Delete Objective</button>
+                  <button
+                    className="action-button delete-button"
+                    onClick={handleDeleteObjective}
+                  >
+                    Delete Objective
+                  </button>
                 </>
               )}
             </div>
@@ -128,7 +147,17 @@ const AddObjectives = () => {
           </div>
         </div>
       </div>
-      <Navbar/>
+      <Navbar />
+      <AddObjectivesPopup
+        isOpen={isCreatePopupOpen}
+        onClose={() => setIsCreatePopupOpen(false)}
+        onSubmit={async (newObjective) => {
+          // Here you would typically call an API to create the objective
+          console.log("Creating new objective:", newObjective);
+          setIsCreatePopupOpen(false);
+          // After successfully creating the objective, you might want to refresh the list of objectives
+        }}
+      />
     </div>
   );
 };
