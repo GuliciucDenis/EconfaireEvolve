96474497b04ba1dacbb717c7859030aca9ee2b49
Leo222004 <brebenelleonardcristian@gmail.com>
2024-09-01 17:54:37 +0300

    Comment added

--- client/src/pages/UserDashboard/UserDashboard.jsx
+++ client/src/pages/UserDashboard/UserDashboard.jsx
@@ -8,7 +8,7 @@ function UserDashboard() {
   return (
     <div className="user-dashboard">
       <div className="user-dashboard-title">
-        <h1>User Dashboard</h1>
+        {/* <h1>User Dashboard</h1> */}
       </div>
       <Background />
       <User />
